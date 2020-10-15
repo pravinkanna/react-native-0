96ea7008bb3a1b5dfd1ee701057291934f843bfb
Pravin Kanna P <mp.pravinkanna@gamil.com>
2020-10-15 08:56:24 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  console.log("App Loaded");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.container} >
+      <Text onPress={() => console.log("text clicked")}>Pravin kanthis is a really really long text i wnat is eve loger htat he uanfun</Text>
+      <Image
+        onClick={() => console.log("photo clicked")}
+        // blurRadius={10}
+        source={{
+          uri: "https://picsum.photos/200/300",
+          width: 200,
+          height: 300
+        }} />
       <StatusBar style="auto" />
-    </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
